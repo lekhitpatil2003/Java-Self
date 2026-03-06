f9d85b1f67b6a96c1c28a62c063258770d3c23e3
Lekhit Patil <patillekhit@gmail.com>
2026-03-06 12:55:40 +0530

    Learn Increment and Decrement operators  with prefix and postfix

--- Operators/IncrementAndDecrementOperator.java
+++ Operators/IncrementAndDecrementOperator.java
@@ -1,20 +1,45 @@
-package Operators;
 public class IncrementAndDecrementOperator {
     public static void main(String[] args) {
+        //Increment / Decrement Operator
         int x = 5;
-
-        // Using increment operator
         x++; // x = x + 1
         System.out.println("After x++: " + x); // Output: 6
 
-        ++x; // x = x + 1
-        System.out.println("After ++x: " + x); // Output: 7
+        int y = 5;
+        y--; // x = x - 1
+        System.out.println("After x--: " + y); // Output: 4
 
-        // Using decrement operator
-        x--; // x = x - 1
-        System.out.println("After x--: " + x); // Output: 6
+        /* 
+            pre and post increment
+        */
+        int j = 7;
+        j++;   //postfix increment. output: 8
+        ++j;    //prefix increment. output: 8
+        
+        System.out.println(j); 
+        
+        // j = 9
 
-        --x; // x = x - 1
-        System.out.println("After --x: " + x); // Output: 5
+        int k = j++; // k = j, j = j + 1
+        System.out.println(j + " , " + k); // j = 10, k = 9
+
+        int l = ++j; // l = j + 1
+        System.out.println(j + " , " + l); // j = 11, k = 11
+
+        /* 
+            pre and post Decrement
+        */
+        int m = 7;
+        m--; // postfix Decrement. output: 6
+        --m; // prefix Decrement. output: 6
+        System.out.println(m); 
+        
+        // m = 5
+
+        int n = m--; // n = m, m = m - 1
+        System.out.println(m + " , " + n); // m = 4, n = 5
+
+        int p = --m; // p = m - 1
+        System.out.println(m + " , " + p); // m = 3, p = 3
     }
 }
