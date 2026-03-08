cef29fe36e1c5d08466eecdb6a03dec01c2f0967
Lekhit Patil <patillekhit@gmail.com>
2026-03-08 17:16:49 +0530

    learn relational operators

--- Operators/RelationalOperators.java
+++ Operators/RelationalOperators.java
@@ -1,3 +1,26 @@
 public class RelationalOperators {
-    
+    public static void main(String[] args) {
+        //  Relational Operators: ==, !=, <, >, <=, >=
+        int a = 5;
+        int b = 10;
+
+        boolean c = (a == b);
+        System.out.println(c); // false
+
+        boolean d = (a != b);
+        System.out.println(d); // true
+
+        boolean e = (a < b);
+        System.out.println(e); // true
+
+        boolean f = (a > b);
+        System.out.println(f); // false
+
+        boolean g = (a <= b);
+        System.out.println(g); // true
+
+        boolean h = (a >= b);
+        System.out.println(h); // false
+    }
 }
+ 
