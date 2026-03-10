65a139bd44197eebf77afb0fe6ad68d8af0c66ed
Lekhit Patil <patillekhit@gmail.com>
2026-03-10 08:09:37 +0530

    bitwise operator file

---
+++ Operators/BitwiseOperators.java
@@ -0,0 +1,8 @@
+public class BitwiseOperators {
+    public static void main(String[] args) {
+        byte a = 2;
+        byte b = 3;
+        int c = (a & b);
+        System.out.println(c);
+    }
+}
