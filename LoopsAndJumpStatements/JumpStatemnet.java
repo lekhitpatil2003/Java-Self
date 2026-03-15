eb4356e0445aadefefb36d27e146fc3ae2454ebb
Lekhit Patil <patillekhit@gmail.com>
2026-03-15 20:13:11 +0530

    learn jump statement

--- LoopsAndJumpStatements/JumpStatemnet.java
+++ LoopsAndJumpStatements/JumpStatemnet.java
@@ -3,13 +3,22 @@ package LoopsAndJumpStatements;
 public class JumpStatemnet {
     public static void main(String[] args) {
         // whetehr a number is prime or not
-        int p = 7;
+        // int p = 9;
+        // int i;
+        // for (i = 1; i < p ; i++) {
+        //     if (p % i == 0) {
+        //         System.out.println("The number is not prime...");
+        //     }
+        // }
+        // if (i == p){
+        //     System.out.println("The number is prime.");
+        // }
 
-        for (int i = 1; i < p ; i++) {
-            if (p % i == 0) {
-                System.out.println("The number is not prime...");
+        for (int i = 1; i <= 10; i++){
+            if (i % 2 == 0) {
+                continue;
             }
+            System.out.println(i);
         }
-
     }
 }
